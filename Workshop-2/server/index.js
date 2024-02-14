a9abdf7a614b903e8c5bea741193b6bead2d05b7
Pamela Murillo <pamelamurilloa@gmail.com>
2024-02-14 16:42:18 -0600

    Workshop done

--- Workshop-2/server/index.js
+++ Workshop-2/server/index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 
 const app = express();
+
 //middlewares
 app.use(bodyParser.json());
 app.use(cors({
@@ -11,13 +12,42 @@ app.use(cors({
 }));
 
 //routes
-app.get('/tipocambio', function (req, res) {
-  res.json({
-    "TipoCompraDolares" : "621",
-    "TipoVentaDolares" : "621",
-    "TipoCompraEuros" : "731.85",
-    "TipoVentaEuros" : "761.9"
-  });
+app.get('/tipocambio', async function (req, res) {
+
+  var myHeaders = new Headers();
+  myHeaders.append("apikey", "REK6cemgPtaLNYfUWR4ciowAn0DEHSmW");
+  
+  var requestOptions = {
+    method: 'GET',
+    redirect: 'follow',
+    headers: myHeaders
+  };
+  let currency = req.query.currency;
+
+  try {
+    let exchange = await fetch(`https://api.apilayer.com/exchangerates_data/convert?to=USD&from=${currency}&amount=1`, requestOptions);
+    exchange = JSON.parse(await exchange.text());
+    
+    console.log(exchange,typeof exchange);
+
+    response = {
+      currency,
+      exchange: exchange.error ? 'desconocido' : exchange.result
+    }
+
+  } catch (error) {
+    response = {
+      currency,
+      exchange: 'desconocido'
+    }
+  }
+
+  res.json(response);
 });
+
+
 //start the app
 app.listen(3001, () => console.log(`BBCR Exchange type service listening on port 3001!`))
+
+
+// REK6cemgPtaLNYfUWR4ciowAn0DEHSmW
