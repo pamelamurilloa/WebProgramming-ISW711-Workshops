8dcc94ebaf19f0ed67d4b3c706cf0b77ce1b0f84
Pamela Murillo <pamelamurilloa@gmail.com>
2024-04-10 18:27:23 -0600

    needs further improvement

--- Workshop-4/server/graphql/resolvers.js
+++ Workshop-4/server/graphql/resolvers.js
@@ -2,34 +2,34 @@ const Course = require('../models/courseModel');
 const Teacher = require('../models/teacherModel');
 
 const resolvers = {
-  Courses: {
-    getAllCourses: async () => {
-      try {
-        const courses = await Course.find();
-        return courses;
-      } catch (error) {
-        throw new Error('Could not fetch courses');
-      }
-    },
-    searchCourses: async (_, { name }) => {
-      try {
-        const courses = await Course.find({ name: { $regex: name, $options: 'i' } });
-        return courses;
-      } catch (error) {
-        throw new Error('Could not search courses');
-      }
-    },
-  },
-  Course: {
-    teacher: async (courseParent) => {
-      try {
-        const teacher = await Teacher.findById(courseParent.teacher);
-        return teacher;
-      } catch (error) {
-        throw new Error('Could not fetch teacher for this course');
-      }
-    }
-  }
-};
+//   Courses: {
+//     getAllCourses: async () => {
+//       try {
+//         const courses = await Course.find();
+//         return courses;
+//       } catch (error) {
+//         throw new Error('Could not fetch courses');
+//       }
+//     },
+//     searchCourses: async (_, { name }) => {
+//       try {
+//         const courses = await Course.find({ name: { $regex: name, $options: 'i' } });
+//         return courses;
+//       } catch (error) {
+//         throw new Error('Could not search courses');
+//       }
+//     },
+//   },
+//   Course: {
+//     teacher: async (courseParent) => {
+//       try {
+//         const teacher = await Teacher.findById(courseParent.teacher);
+//         return teacher;
+//       } catch (error) {
+//         throw new Error('Could not fetch teacher for this course');
+//       }
+//     }
+//   }
+// }; need further clarification for this, will search in the following days
 
 module.exports = resolvers;
